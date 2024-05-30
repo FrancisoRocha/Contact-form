8a4543b00d6d5a7b1f7eb4e44dba350c799ff7cd
Francisco Rocha <122055396+FrancisoRocha@users.noreply.github.com>
2024-05-29 23:55:08 -0600

    first commit

---
+++ src/body/body.js
@@ -0,0 +1,9 @@
+import './body.css'
+
+function backColor (){
+    return(
+        <body></body>
+    )
+};
+
+export default backColor;
---
+++ src/card/card.js
@@ -0,0 +1,13 @@
+import './card.css'
+
+function cards (){
+    return(
+        <div className="cards">
+            <div className="card-container">
+                <h2 className="title">Contact Us</h2>
+            </div>
+        </div>
+    )
+};
+
+export default cards;
